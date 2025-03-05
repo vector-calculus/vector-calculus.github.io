98170b82fbc45e5a3f55dd055568924e23074ba8
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2025-03-05 17:52:11 +1000

    new applet

---
+++ applets/p5js/flow-field-noise-white/main.js
@@ -0,0 +1,183 @@
+/* 
+ Original code by 
+ Johan Karlsson (https://codepen.io/DonKarlssonSan/pen/aLRVbw)
+ 
+ This version by Juan Carlos Ponce Campuzano, 15-Nov-2022
+ Update: 
+ https://jcponce.github.io
+*/
+
+class Particle {
+  constructor(x, y) {
+    this.pos = new Vector(x, y);
+    this.vel = new Vector(Math.random()*2-1, Math.random()*2-1);
+    this.acc = new Vector(0, 0);
+    this.size = 8;
+  }
+  
+  move(acc) {
+    if(acc) {
+      this.acc.addTo(acc);
+    }
+    this.vel.addTo(this.acc);
+    this.pos.addTo(this.vel);
+    if(this.vel.getLength() > 1) {
+      this.vel.setLength(1);
+    }
+    this.acc.setLength(0);
+  }
+  
+  draw() {
+    //ctx.fillRect(this.pos.x, this.pos.y, this.size, this.size);
+    ctx.beginPath();
+    ctx.ellipse(this.pos.x, this.pos.y, this.size/2, this.size/2,  0, 0, 2 * Math.PI);
+    //ctx.stroke();
+    ctx.fill();
+  }
+  
+  wrap() {
+    if(this.pos.x > w) {
+      this.pos.x = 0;
+    } else if(this.pos.x < -this.size) {
+      this.pos.x = w - 1;
+    }
+    if(this.pos.y > h) {
+      this.pos.y = 0;
+    } else if(this.pos.y < -this.size) {
+      this.pos.y = h - 1;
+    }
+  }
+}
+
+let canvas;
+let ctx;
+let field;
+let w, h;
+let size;
+let columns;
+let rows;
+let noiseZ;
+let particles;
+let hue;
+
+function setup() {
+  size = 20;
+  hue = 0;
+  noiseZ = 0;
+  canvas = document.querySelector("#canvas");
+  ctx = canvas.getContext("2d");
+  reset();
+  window.addEventListener("resize", reset);  
+}
+
+function initParticles() {
+  particles = [];
+  let numberOfParticles = w * h / 1500;
+  for(let i = 0; i < numberOfParticles; i++) {
+    let particle = new Particle(Math.random() * w, Math.random() * h);
+    particles.push(particle);
+  }
+}
+
+function initField() {
+  field = new Array(columns);
+  for(let x = 0; x < columns; x++) {
+    field[x] = new Array(columns);
+    for(let y = 0; y < rows; y++) {
+      let v = new Vector(0, 0);
+      field[x][y] = v;
+    }
+  }
+}
+
+function calculateField() {
+  for(let x = 0; x < columns; x++) {
+    for(let y = 0; y < rows; y++) {
+      let angle = noise.simplex3(x/20, y/20, noiseZ) * Math.PI * 2;
+      let length = noise.simplex3(x/40 + 40000, y/40 + 40000, noiseZ) * 0.5;
+      field[x][y].setLength(length);
+      field[x][y].setAngle(angle);
+    }
+  }
+}
+
+function reset() {
+  w = canvas.width = window.innerWidth;
+  h = canvas.height = window.innerHeight;
+  ctx.strokeStyle = "grey";
+  noise.seed(Math.random());
+  columns = Math.round(w / size) + 1;
+  rows = Math.round(h / size) + 1;
+  initParticles();
+  initField();
+}
+
+function draw(now) {
+  requestAnimationFrame(draw);
+  calculateField();
+  noiseZ = now * 0.0002;
+  drawBackground();
+  drawFlowField();
+  drawParticles();
+}
+
+function drawBackground() {
+  ctx.fillStyle = "rgb(249, 250, 251)"; // Light grey background
+  ctx.fillRect(0, 0, w, h);
+}
+
+function drawParticles() {
+  hue += 0.5;
+  ctx.fillStyle = `hsla(${hue}, 50%, 50%, 0.96)`;
+  particles.forEach(p => {
+    p.draw();
+    let pos = p.pos.div(size);
+    let v;
+    if(pos.x >= 0 && pos.x < columns && pos.y >= 0 && pos.y < rows) {
+      v = field[Math.floor(pos.x)][Math.floor(pos.y)];
+    }
+    p.move(v);
+    p.wrap();
+  });
+}
+
+function drawFlowField() {
+  ctx.strokeStyle = "grey"; // Set stroke color to grey
+  ctx.lineWidth = 1.8; // Set line thickness
+
+  for (let x = 0; x < columns; x++) {
+    for (let y = 0; y < rows; y++) {
+      ctx.beginPath();
+      let x1 = x * size;
+      let y1 = y * size;
+      let x2 = x1 + field[x][y].x * size * 2;
+      let y2 = y1 + field[x][y].y * size * 2;
+
+      // Draw the main line
+      ctx.moveTo(x1, y1);
+      ctx.lineTo(x2, y2);
+      ctx.stroke();
+
+      // Draw the arrowhead
+      let arrowSize = 5; // Size of arrowhead
+      let angle = Math.atan2(y2 - y1, x2 - x1); // Calculate the angle of the vector
+
+      ctx.beginPath();
+      ctx.moveTo(x2, y2);
+      ctx.lineTo(
+        x2 - arrowSize * Math.cos(angle - Math.PI / 6),
+        y2 - arrowSize * Math.sin(angle - Math.PI / 6)
+      ); // Left wing
+      ctx.moveTo(x2, y2);
+      ctx.lineTo(
+        x2 - arrowSize * Math.cos(angle + Math.PI / 6),
+        y2 - arrowSize * Math.sin(angle + Math.PI / 6)
+      ); // Right wing
+      ctx.stroke();
+    }
+  }
+}
+
+
+setup();
+draw(performance.now());
