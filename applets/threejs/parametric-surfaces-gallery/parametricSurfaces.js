3668729139516a865c2ba68287509bf66b76c4c2
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2025-03-12 16:08:54 +1000

    Update parametricSurfaces.js

--- applets/threejs/parametric-surfaces-gallery/parametricSurfaces.js
+++ applets/threejs/parametric-surfaces-gallery/parametricSurfaces.js
@@ -10,6 +10,17 @@ function parametricSphere(u, v, target) {
     target.set(x, y, z);
 }
 
+// **Sea Shell Surface**
+function seaShell(u, v, target) {
+    u *= 20;
+    v *= 2 * Math.PI;
+    const f = Math.exp(u / (Math.PI * 6 * 1)) - 1;
+    const scale = 0.4;
+    let x = scale * ( 2 * f * Math.cos(u) * Math.cos(v / 2) * Math.cos(v / 2));
+    let y = scale * ( 2 * (-f) * Math.sin(u) * Math.cos(v / 2) * Math.cos(v / 2));
+    let z = scale * ( 4 - Math.exp(u / (options.a)) - Math.sin(v) + Math.exp(u / (Math.PI * 6 * 1)) * Math.sin(v));
+    target.set(x, y, z);
+  }
 
 
 // **Another Example Surface**
