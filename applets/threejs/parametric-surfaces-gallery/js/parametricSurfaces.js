de006be33812c2bda3dbb03ac1d5c9c9f9e0e520
Juan Carlos Ponce Campuzano <37394697+jcponce@users.noreply.github.com>
2025-03-20 14:02:51 +1000

    ok

--- applets/threejs/parametric-surfaces-gallery/js/parametricSurfaces.js
+++ applets/threejs/parametric-surfaces-gallery/js/parametricSurfaces.js
@@ -109,14 +109,14 @@ export function enneperSurface(u, v, target) {
     target.set(x, y, z);
 }
 
-function projection(w, x, y, z){
+function projection(w, x, y, z) {
     let a, b, c;
 
     a = w / (1 - z);
     b = x / (1 - z);
     c = y / (1 - z);
 
-    return {x: a, y: b, z: c}
+    return { x: a, y: b, z: c }
 }
 
 export function figure8knotSurface(u, v, target) {
@@ -261,9 +261,9 @@ export function mobiusSurface(u, v, target, R, vComponent) {
  * Rose-shaped by Paul Nylander
  * https://nylander.wordpress.com/2006/06/21/rose-shaped-parametric-surface/
  */
-export function roseSurface(u, v, target, vMin) {
+export function roseSurface(u, v, target, uMin) {
     u = 1 * u;
-    v = vMin + (20 * PI - (vMin)) * v;
+    v = uMin + (20 * PI - (uMin)) * v;
 
     let alpha, s, c, beta, r, x, y, z;
     alpha = 1 - 0.5 * pow(5 / 4 * pow(1 - ((3.6 * v) % (2 * PI)) / PI, 2) - 0.25, 2);
@@ -367,19 +367,18 @@ export function torusbraidedSurface(u, v, target, a, n, r, R, uComponent, vCompo
     target.set(x, y, z);
 }
 
-export function torusknotSurface(u, v, target, size) {
+export function torusknotSurface(u, v, target, size, translate) {
     u = 4 * PI * u;
     v = 2 * PI * v;
 
     let NN = 1.5;
     let AA = 0.6;
-    let w =  3 * cos(u)/ 4;
-    const constant = -0;
+    let w = 3 * cos(u) / 4;
 
     let XX, YY, ZZ;
-    XX = -size * (cos(u) * cos(v) + 3 * cos(u) * (1.5 + sin(1.5 * u) / 2)) + constant;
-    YY = size * (sin(u) * cos(v) + 3 * sin(u) * (1.5 + sin(1.5 * u) / 2)) ;
-    ZZ = size * (sin(v) + 2 * cos(1.5 * u)) ;
+    XX = -size * (cos(u) * cos(v) + 3 * cos(u) * (1.5 + sin(1.5 * u) / 2)) + translate;
+    YY = size * (sin(u) * cos(v) + 3 * sin(u) * (1.5 + sin(1.5 * u) / 2));
+    ZZ = size * (sin(v) + 2 * cos(1.5 * u));
     let norm2 = pow(XX, 2) + pow(YY, 2) + pow(ZZ, 2);
 
     let x = 0.5 * XX / norm2;
@@ -423,3 +422,16 @@ export function torusumbilicSurface(u, v, target, uComponent, vComponent) {
 
     target.set(x, y, z);
 }
+
+export function trefoilknotSurface(u, v, target, uComponent, vComponent, size, tx, ty, tz) {
+    u = uComponent * u;
+    v = vComponent * v;
+
+    let x, y, z;
+    x = -size * (cos(u) * cos(v) + 3 * cos(u) * (1.5 + sin(1.5 * u) / 2)) + tx;
+    y = size * (sin(u) * cos(v) + 3 * sin(u) * (1.5 + sin(1.5 * u) / 2)) + ty;
+    z = size * (sin(v) + 2 * cos(1.5 * u)) + tz;
+
+    target.set(x, y, z);
+}
+
--- applets/threejs/parametric-surfaces-gallery/js/torusknotMain.js
+++ applets/threejs/parametric-surfaces-gallery/js/torusknotMain.js
@@ -1,7 +1,7 @@
 import * as THREE from 'three';
 import { GUI } from 'three/addons/libs/lil-gui.module.min.js';
 import { ParametricGeometry } from 'three/addons/geometries/ParametricGeometry.js';
-import { torusknotSurface as parametricSurface } from './parametricSurfaces.js';
+import { torusknotSurface as parametricSurface, sphereSurface, trefoilknotSurface } from './parametricSurfaces.js';
 import { createMaterials } from './materials.js';
 import { setupScene } from './sceneSetup.js';
 import { commonUI } from './commonUI.js';
@@ -11,8 +11,9 @@ const gui = new GUI();
 const options = {
     material: 'Matcap', // Default material
     mesh: false,
-    size: 0.304,
-    autoRotate: true,
+    size: 0.12,
+    translate: 2,
+    autoRotate: false,
     color: '#049ef4',
     colorBackground: '#000000'
 };
@@ -25,15 +26,56 @@ const { scene, camera, renderer, controls } = setupScene(canvas);
 const materials = createMaterials(options);
 
 // Geometry
-const meshRes = { x: 400, y: 100 };
-const geoScale = { x: 3, y: 3, z: 3 };
-let geometry = new ParametricGeometry((u, v, target) => parametricSurface(u, v, target, options.size), meshRes.x, meshRes.y);
-geometry.rotateX(-0.5);
+const meshRes = { x: 500, y: 100 };
+const geoScale = { x: 1, y: 1, z: 1 };
+let geometry = new ParametricGeometry((u, v, target) => parametricSurface(u, v, target, options.size, options.translate), meshRes.x, meshRes.y);
+geometry.rotateX(Math.PI/2);
 geometry.scale(geoScale.x, geoScale.y, geoScale.z);
+
+let geometrySphere = new ParametricGeometry((u, v, target) => sphereSurface(u, v, target, 1), 20, 20);
+geometrySphere.rotateX(Math.PI/2);
+geometrySphere.scale(geoScale.x, geoScale.y, geoScale.z);
+
+// Create wireframe for sphere
+const wireframeGeometrySphere = new THREE.WireframeGeometry(geometrySphere);
+const wireframeMaterialSphere = new THREE.LineBasicMaterial({ color: 0xbfbfbf }); // White wireframe
+const wireframeSphere = new THREE.LineSegments(wireframeGeometrySphere, wireframeMaterialSphere);
+
+
+let geometryKnot = new ParametricGeometry((u, v, target) => trefoilknotSurface(u, v, target, 4 * Math.PI, 2 * Math.PI, options.size, options.translate, 0, 0), 64, 64);
+geometryKnot.rotateX(Math.PI/2);
+geometryKnot.scale(geoScale.x, geoScale.y, geoScale.z);
 
 // Mesh
 let mesh = new THREE.Mesh(geometry, materials.matcapMaterial);
 scene.add(mesh);
+
+let materialSphere = new THREE.MeshPhongMaterial({
+    color: '#ffffff', 
+    //shininess: 100, 
+    side: THREE.DoubleSide, 
+    transparent: true,
+    opacity: 0.1, 
+    wireframe: true
+});
+
+let meshSphere = new THREE.Mesh(geometrySphere, materialSphere);
+scene.add(meshSphere);
+scene.add(wireframeSphere);
+
+
+let materialKnot = new THREE.MeshPhongMaterial({
+    color: '#00ffff', 
+    shininess: 200, 
+    side: THREE.DoubleSide, 
+    //transparent: true,
+    //opacity: 1, 
+    //wireframe: true
+});
+
+let meshKnot = new THREE.Mesh(geometryKnot, materialKnot);
+scene.add(meshKnot);
+
 scene.background = new THREE.Color('#000000'); // Initial Black background
 
 // Wireframe mesh
@@ -42,16 +84,49 @@ let wireframeMesh = new THREE.Mesh(geometry, materials.wireframeMaterial);
 // GUI controls
 commonUI(gui, options, scene, materials, mesh, wireframeMesh, controls); // Call commonUI with necessary arguments
 
-gui.add(options, 'size', 0, 2, 0.001).onChange(() => {
+gui.add(options, 'size', 0.01, 2, 0.01).name('Dilate').onChange(() => {
     geometry.dispose();
-    geometry = new ParametricGeometry((u, v, target) => parametricSurface(u, v, target, options.size), meshRes.x, meshRes.y);
-    geometry.rotateX(-Math.PI);
+    geometry = new ParametricGeometry((u, v, target) => parametricSurface(u, v, target, options.size, options.translate), meshRes.x, meshRes.y);
+    geometry.rotateX(Math.PI/2);
+    geometry.scale(geoScale.x, geoScale.y, geoScale.z);
+    mesh.geometry = geometry;
+
+    wireframeMesh.geometry = geometry;
+
+    // Update the trefoil knot surface
+    geometryKnot.dispose();
+    geometryKnot = new ParametricGeometry((u, v, target) => trefoilknotSurface(u, v, target, 4 * Math.PI, 2 * Math.PI, options.size, options.translate, 0, 0), 64, 64);
+    geometryKnot.rotateX(Math.PI / 2);
+    geometryKnot.scale(geoScale.x, geoScale.y, geoScale.z);
+    meshKnot.geometry = geometryKnot;
+    
+});
+
+gui.add(options, 'translate', -3, 3, 0.01).name('Translate').onChange(() => {
+    geometry.dispose();
+    geometry = new ParametricGeometry((u, v, target) => parametricSurface(u, v, target, options.size, options.translate), meshRes.x, meshRes.y);
+    geometry.rotateX(Math.PI/2);
     geometry.scale(geoScale.x, geoScale.y, geoScale.z);
     mesh.geometry = geometry;
     wireframeMesh.geometry = geometry;
+
+    // Update the trefoil knot surface
+    geometryKnot.dispose();
+    geometryKnot = new ParametricGeometry((u, v, target) => trefoilknotSurface(u, v, target, 4 * Math.PI, 2 * Math.PI, options.size, options.translate, 0, 0), 64, 64);
+    geometryKnot.rotateX(Math.PI / 2);
+    geometryKnot.scale(geoScale.x, geoScale.y, geoScale.z);
+    meshKnot.geometry = geometryKnot;
 });
 
-controls.autoRotate = true;
+gui.open();
+
+const gridHelper = new THREE.GridHelper(10, 10);
+scene.add(gridHelper);
+
+const axesHelper = new THREE.AxesHelper(1);
+scene.add(axesHelper);
+
+controls.autoRotate = false;
 controls.autoRotateSpeed = - 0.9;
 
 // Animation loop
